3a4f5013187c57dbf938435f52ae516260a413f6
Felipech09 <felipecamhipolito@gmail.com>
2026-06-05 15:57:45 -0300

    Consertando erros de lógica

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken')
+
 //verrifica se o usuário tem o token
 function verificarToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) return res.status(401).json({ error: 'Acesso negado, faça o login' })
-        
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ error: 'Token inválido ou expirado' })
+    
 
+jwt.verify(token.split(" ")[1], process.env.JWT_SECRET || 'secret123', (err, decoded) => {
+        if (err) return res.status(401).send({ message: "Não autorizado!" });
+
+        
         //salva os dados dos usuários para usar nas proximas funçoes
-        req.userId = decoded.id
-        rqe.userRole = decoded.role
-        next()
-    })
+        req.userId = decoded.id;
+        req.userRole = decoded.role;
+        req.mustChangePassword = decoded.mustChangePassword;
+        next();
+    });
 }
 
 // verificar se é gerente
-funcion apenasGerente(req, res, next) {
-    if (req.userRole !== 'gerente') {
-        return res.status(403).json({ error: 'Acesso negado para o seu cargo' })
+function isManager(req, res, next) {
+    if (req.userRole === 'manager') {
+        next();
+    } else {
+        res.status(403).send({ message: "Requer privilégios de Gerente!" });
     }
-    next()
 }
 
-modules.exports = { verificarToken, apenasGerente }
+module.exports = { verifyToken, isManager };
